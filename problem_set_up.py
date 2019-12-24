4e568ccd1d634926a3c0a4c1d54c17f59c0edbec
tdw75 <tdw75@outlook.com>
2019-12-24 11:42:13 +0100

    Changed from a functional structure to an object oriented one that can be inherited by the various algorithm implementations

--- problem_set_up.py
+++ problem_set_up.py
@@ -6,21 +6,6 @@ def euclidean_distance(point1, point2):
     return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
 
 
-def parse_input_data(input_data):
-    Point = namedtuple("Point", ['x', 'y'])
-    lines = input_data.split('\n')
-
-    node_count = int(lines[0])
-    points = []
-
-    for i in range(1, node_count + 1):
-        line = lines[i]
-        parts = line.split()
-        points.append(Point(float(parts[0]), float(parts[1])))
-
-    return points
-
-
 def distance_matrix(points):
     distances = {}
     for point1 in points:
@@ -33,5 +18,30 @@ def distance_matrix(points):
     return distances
 
 
-def calculate_tour_length():
-    pass
+class TspSetUp:
+    def __init__(self, input_data):
+        self.node_count = None
+        self.nodes = None
+        self.coordinates = self.parse_input_data(input_data)
+        self.dist_matrix = distance_matrix(self.coordinates)
+
+    def parse_input_data(self, input_data):
+        Point = namedtuple("Point", ['x', 'y'])
+        lines = input_data.split('\n')
+
+        self.node_count = int(lines[0])
+        self.nodes = list(range(self.node_count))
+        points = []
+
+        for i in range(1, self.node_count + 1):
+            line = lines[i]
+            parts = line.split()
+            points.append(Point(float(parts[0]), float(parts[1])))
+
+        return points
+
+    def calculate_tour_length(self):
+        pass
+
+    def save_solution(self):
+        pass
