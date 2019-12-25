f0100187a318f705e1f40c42698ddfa5f085c6b6
tdw75 <tdw75@outlook.com>
2019-12-25 15:36:01 +0100

    Made minor changes to the problem set up class

--- problem_set_up.py
+++ problem_set_up.py
@@ -8,40 +8,56 @@ def euclidean_distance(point1, point2):
 
 def distance_matrix(points):
     distances = {}
+    node = 0
     for point1 in points:
-        distances[point1] = []
+
+        distances[node] = []
 
         for point2 in points:
             distance = euclidean_distance(point1, point2)
-            distances[point1].append(distance)
+            distances[node].append(distance)
+
+        node += 1
 
     return distances
 
 
+def parse_input_data(input_data):
+    Point = namedtuple("Point", ['x', 'y'])
+    lines = input_data.split('\n')
+
+    node_count = int(lines[0])
+    nodes = list(range(node_count))
+    points = []
+
+    for i in range(1, node_count + 1):
+        line = lines[i]
+        parts = line.split()
+        points.append(Point(float(parts[0]), float(parts[1])))
+
+    return points, node_count, nodes
+
+
 class TspSetUp:
     def __init__(self, input_data):
-        self.node_count = None
-        self.nodes = None
-        self.coordinates = self.parse_input_data(input_data)
+        self.coordinates, self.node_count, self.nodes = parse_input_data(input_data)
         self.dist_matrix = distance_matrix(self.coordinates)
-
-    def parse_input_data(self, input_data):
-        Point = namedtuple("Point", ['x', 'y'])
-        lines = input_data.split('\n')
-
-        self.node_count = int(lines[0])
-        self.nodes = list(range(self.node_count))
-        points = []
-
-        for i in range(1, self.node_count + 1):
-            line = lines[i]
-            parts = line.split()
-            points.append(Point(float(parts[0]), float(parts[1])))
-
-        return points
+        self.obj_value = None
 
     def calculate_tour_length(self):
         pass
 
     def save_solution(self):
         pass
+
+    def plot_tour(self):
+        pass
+
+
+if __name__ == "__main__":
+    with open('data\\tsp_5_1', 'r') as input_data_file:
+        input_data = input_data_file.read()
+
+    tsp = TspSetUp(input_data)
+    print(tsp.coordinates)
+    print(tsp.dist_matrix.keys())
