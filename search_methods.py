d0b0742d26281fe92b33b2f645368f400035ab85
tdw75 <tdw75@outlook.com>
2019-12-27 14:09:29 +0100

    Initialised methods for 2-Opt class

--- search_methods.py
+++ search_methods.py
@@ -6,17 +6,29 @@ class TwoOpt(TspSetUp):
     def __init__(self, input_data):
         super().__init__(input_data)
 
+    def identify_orientation(self):
+        pass
 
-def IteratedGreedy(TspSetUp):
+    def find_intersecting_edges(self):
+        pass
+
+    def two_opt_swap(self):
+        pass
+
+    def search(self):
+        pass
+
+
+class IteratedGreedy(TspSetUp):
     def __init__(self, input_data):
         super().__init__(input_data)
 
 
-def SimulatedAnnealing(TspSetUp):
+class SimulatedAnnealing(TspSetUp):
     def __init__(self, input_data):
         super().__init__(input_data)
 
 
-def TabuSearch(TspSetUp):
+class TabuSearch(TspSetUp):
     def __init__(self, input_data):
         super().__init__(input_data)
