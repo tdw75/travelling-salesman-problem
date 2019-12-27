4f483865fb6816c1de67d1a9093db52999728403
tdw75 <tdw75@outlook.com>
2019-12-27 13:43:57 +0100

    Initialised script containing local search methods

---
+++ search_methods.py
@@ -0,0 +1,22 @@
+from problem_set_up import *
+import time
+
+
+class TwoOpt(TspSetUp):
+    def __init__(self, input_data):
+        super().__init__(input_data)
+
+
+def IteratedGreedy(TspSetUp):
+    def __init__(self, input_data):
+        super().__init__(input_data)
+
+
+def SimulatedAnnealing(TspSetUp):
+    def __init__(self, input_data):
+        super().__init__(input_data)
+
+
+def TabuSearch(TspSetUp):
+    def __init__(self, input_data):
+        super().__init__(input_data)
